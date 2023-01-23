296a5117aa42b648af4beee4e0b647e382fed972
wiemtrifi <wiem.trifi@esprit.tn>
2023-01-23 12:41:28 +0100

    Web

---
+++ Examen.Web/Controllers/NFlowerController.cs
@@ -0,0 +1,125 @@
+﻿
+using Examen.ApplicationCore.Domain;
+using Examen.ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Examen.Web.Controllers
+{
+    public class NFlowerController : Controller
+    {
+        IServiceBouquet bouq;
+        IserviceNFlower nf;
+        // GET: FlightController
+        public NFlowerController(IServiceBouquet bouq, IserviceNFlower nf)
+        {
+            this.bouq = bouq;
+            this.nf = nf;
+        }
+      
+        public ActionResult Index()
+        {
+          
+                return View(nf.GetAll());
+        
+          
+        }
+
+        // GET: ClientController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ClientController/Create
+        public ActionResult Create()
+        {
+            ViewBag.bouquets = new SelectList(bouq.GetAll(), "BouquetCode","BouquetType");
+            return View();
+        }
+
+        // POST: ClientController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NaturalFlower nff)
+        {
+            try
+            {
+              
+                nf.Add(nff);
+                nf.Commit();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+     /*   // GET: ClientController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            ViewBag.conseillerFk = new SelectList(scons.GetAll(), "conseillerId", "conseillerName");
+            return View(sc.GetById(id));
+        }
+
+        // POST: ClientController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Client collection, IFormFile photo)
+        {
+            
+            try
+            {
+                if (photo == null || photo.Length == 0)
+                {
+                    return Content("File not selected");
+                }
+                if (photo != null)
+                {
+                    var path = Path.Combine(Directory.GetCurrentDirectory(),
+
+                    "wwwroot", "Uploads", photo.FileName);
+
+                    Stream stream = new FileStream(path, FileMode.Create);
+                    photo.CopyTo(stream);
+                    collection.photo = photo.FileName;
+
+                }
+               
+                sc.Update(collection);
+                sc.Commit(); 
+               
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ClientController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(sc.GetById(id));
+        }
+
+        // POST: ClientController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                sc.Delete(sc.GetById(id));
+                sc.Commit();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+     */
+    }
+}
---
+++ Examen.Web/Program.cs
@@ -0,0 +1,37 @@
+using Examen.ApplicationCore.Interfaces;
+using Examen.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Examen.ApplicationCore.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+// Injection de dépendance
+builder.Services.AddDbContext<DbContext, ExamenContext>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IserviceNFlower, serviceNFlower>();
+builder.Services.AddScoped<IServiceBouquet, ServiceBouquet>();
+builder.Services.AddSingleton<Type>(t => typeof(GenericRepository<>));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
